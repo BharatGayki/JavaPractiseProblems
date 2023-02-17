35f697f33cbaf5f85f122cfe75e46943c4a40a91
Bharat Gayki <bhaaratgayki@gmail.com>
2023-02-17 05:54:15 +0530

    Area of Triangle

---
+++ question_1.java
@@ -0,0 +1,27 @@
+///  Jai Shree Ram
+//write a java program to compute the parameter and area of rectangle with a height of 7 inch and with oif 5 inch
+import java.util.*;
+class question_1{
+
+public static void main(String [] argu){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Jai Hanuman");
+int a =7 , b = 5;
+System.out.println("Height is "+a+"Width is "+b);
+
+int c = a*b;
+
+
+System.out.println("Area is "+c);
+
+
+
+
+
+
+
+}
+
+}
