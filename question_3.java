ebdec784454db7801932dba1eeacb50586612a9d
Bharat Gayki <bhaaratgayki@gmail.com>
2023-02-17 10:43:41 +0530

    Addition of num

---
+++ question_3.java
@@ -0,0 +1,21 @@
+// jai shree ram
+
+//write a java program to to compute the parameter and area of the circle with the radius of 6 inch
+
+class question_2{
+
+public static void main(String [] args){
+
+
+int rai = 6;
+
+float area =(float) 3.14*rai*rai;
+
+System.out.println("Area of circle is "+area);
+
+
+}
+
+
+
+}
