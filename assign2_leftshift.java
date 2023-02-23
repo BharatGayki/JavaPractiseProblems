c909e90c8f419d1c9dc438d469c2d766132a352d
Bharat Gayki <bhaaratgayki@gmail.com>
2023-02-23 05:45:58 +0530

    23Feb Questions

---
+++ assign2_leftshift.java
@@ -0,0 +1,32 @@
+// Jai Shree Ram
+
+//Write a Java program to shift given data by two bits to the left.
+
+import java.util.*;
+
+class assign2_leftshift{
+
+public static void main(String [] argus){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter first num: ");
+int a = sc.nextInt();
+
+int b;
+
+b = a<<2;
+
+System.out.println("Left shift " +b);
+
+
+
+}
+
+
+
+
+
+
+
+}
