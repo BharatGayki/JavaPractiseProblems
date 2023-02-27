7780bbf93e5b409897d36eb0b46a27fc4202a583
Bharat Gayki <bhaaratgayki@gmail.com>
2023-02-27 21:46:06 +0530

    Array Programs

---
+++ Array_Programs/Array2.java
@@ -0,0 +1,44 @@
+// Jai Shree Rqam
+
+import java.util.*;
+
+class Array2{
+
+public static void main(String [] args){
+
+	Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter the size");
+int size = sc.nextInt();
+
+int a[] = new int [length];
+
+System.out.println("Enter the elements"+length);
+for(int i=0;i<a.length;i++){
+
+	a[i] = sc.nextInt();
+
+}
+for	(int i=0;i<a.length/2;i++)
+{
+int	t=a[i];
+a[i]=a[a.length-1-i];	// main logic
+a[a.length-1-i) = t;
+}
+
+System.out.println("Jai Bajrangbali");
+
+System.out.println("After Reverse of an elements");
+for	(intv i=0;i<a.length;i++)
+	 {
+
+	 	 System.out.println	("elements"+a[i]);
+	 }
+
+
+
+}
+
+
+
+}
